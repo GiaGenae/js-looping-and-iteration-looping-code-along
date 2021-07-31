666081c744cd81e80b7c96950d03181de82fb256
Gia Jennings <gjennings.co@gmail.com>
2021-07-31 19:27:26 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+
+const names = ["Lisa", "Kaitlin", "Jan"]
+
+function writeCards(names) {
+    let cards = []
+    for (let i = 0; i < names.length; i++) {
+      cards.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    }
+  
+    return cards;
+}
+
+function countDown() {
+    let countDown = 10;
+    while (countDown >= 0) {
+      console.log(countDown);
+      countDown -= 1;
+    }
+    console.log
+}
